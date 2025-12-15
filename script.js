927dbdb29c5dc7e9b6586e6089cbbf1b0c2f452f
unknown <VictorOre@users.noreply.github.com>
2025-12-15 15:00:43 +0100

    init

---
+++ script.js
@@ -0,0 +1,97 @@
+/* Uppgift 1 */
+
+// Globala konstanter och variabler
+const wordList = [
+  "BLOMMA",
+  "LASTBIL",
+  "SOPTUNNA",
+  "KÖKSBORD",
+  "RADIOAPPARAT",
+  "VINTER",
+  "SOMMAR",
+  "DATORMUS",
+  "LEJON",
+  "ELEFANT",
+  "JULTOMTE",
+  "SKOGSHYDDA",
+  "BILNUMMER",
+  "BLYERTSPENNA",
+  "SUDDGUMMI",
+  "KLÄDSKÅP",
+  "VEDSPIS",
+  "LJUSSTAKE",
+  "SKRIVBORD",
+  "ELDGAFFEL",
+  "STEKPANNA",
+  "KASTRULL",
+  "KAFFEBRYGGARE",
+  "TALLRIK",
+  "SOFFBORD",
+  "TRASMATTA",
+  "FLYGPLAN",
+  "FLYGPLATS",
+  "TANGENTBORD",
+  "DATORSPEL",
+  "WEBBPLATS",
+  "TELEFON",
+  "STJÄRNA",
+  "KANELBULLE",
+  "SEMLA",
+  "ÄPPELPAJ",
+  "BLÅBÄR",
+  "LINGONSYLT",
+  "TRAKTOR",
+  "CYKELKEDJA",
+  "BOKHYLLA",
+  "BOKSTAV",
+  "GRILLPLATS",
+  "SOLSTOL",
+  "BADPLATS",
+  "SNÖGUBBE",
+  "PARAPLY",
+]; // Lista (array) med ord som ska väljas slumpmässigt
+
+const startButton = document.querySelector("#start"); // button-element för startknappen
+const infoElement = document.querySelector("#message"); // div-element för meddelanden
+const letterButtons = document.querySelectorAll("#letterButtons button"); // Array med button-element för bokstavsknapparna
+const hangmanImage = document.querySelector("#hangman"); // img-elementet för bilder på galgen och gubben
+let guessNr; // Nummer för aktuell bild som visas (0-6)
+let randomWord; // Textsträng med det ord som slumpmässigt väljs ur wordList
+let boxElements; // Array med span-element för bokstäverna i ordet
+let startTime; // Tid då spelet startas
+
+// --------------------------------------------------
+// Global setup
+
+// TODO:
+
+// --------------------------------------------------
+// Initiera ett nytt spel. Visa första bilden (h0.png),
+// sätt bildnummer till 0, inaktivera startknapp och aktivera bokstavsknappar.
+function startGame() {
+  //TODO:
+}
+
+// --------------------------------------------------
+// Ett ord väljs slumpmässigt. Visa en ruta för varje bokstav i ordet
+// Ordet ska ej vara samma som föregående ord, om man spelar flera gånger
+function selectRandomWord() {
+  //TODO:
+}
+
+// --------------------------------------------------
+// Kontrollera om bokstaven finns i ordet och skriv i så fall ut den.
+// Om bokstaven ej finns, uppdateras bilden med galgen och gubben
+// Om alla bokstäver är gissade eller om den sista bilden visades, avslutas spelet
+function guessLetter(event) {
+  // event är klickhändelsen från en bokstavsknapp
+  //TODO:
+}
+
+// --------------------------------------------------
+// Avsluta spelet genom att skriva ut ett meddelande och
+// sedan aktivera startknappen och inaktivera bokstavsknapparna
+function endGame(manHanged) {
+  // manHanged är true eller false
+  //TODO:
+}
